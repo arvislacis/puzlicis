0d65aad2dedc3649d6cc2abcf896c6257b81b7f1
Arvis Lācis <arvis.lacis@gmail.com>
2013-12-23 19:52:39 +0200

    Initial Commit
    Pievienotā funkcionalitāte, izmaiņas:
    - izveidots programmas noformējuma pamats;
    - sākuma attēla ģenerēšana (sadalīšana puzles gabaliņos) bez papildu
    parametriem vai iespējām.

---
+++ galvena_forma.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace puzlicis
+{
+    partial class galvena_forma
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.statusa_josla = new System.Windows.Forms.StatusStrip();
+            this.izvelne = new System.Windows.Forms.MenuStrip();
+            this.taimeris = new System.Windows.Forms.Timer(this.components);
+            this.panelis_spele = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // statusa_josla
+            // 
+            this.statusa_josla.Location = new System.Drawing.Point(0, 390);
+            this.statusa_josla.Name = "statusa_josla";
+            this.statusa_josla.Size = new System.Drawing.Size(734, 22);
+            this.statusa_josla.TabIndex = 0;
+            this.statusa_josla.Text = "Statusa josla";
+            // 
+            // izvelne
+            // 
+            this.izvelne.Location = new System.Drawing.Point(0, 0);
+            this.izvelne.Name = "izvelne";
+            this.izvelne.Size = new System.Drawing.Size(734, 24);
+            this.izvelne.TabIndex = 1;
+            this.izvelne.Text = "Izvēlne";
+            // 
+            // taimeris
+            // 
+            this.taimeris.Interval = 1000;
+            // 
+            // panelis_spele
+            // 
+            this.panelis_spele.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panelis_spele.Location = new System.Drawing.Point(12, 27);
+            this.panelis_spele.Name = "panelis_spele";
+            this.panelis_spele.Size = new System.Drawing.Size(480, 360);
+            this.panelis_spele.TabIndex = 5;
+            this.panelis_spele.MouseClick += new System.Windows.Forms.MouseEventHandler(this.panelis_spele_MouseClick);
+            // 
+            // galvena_forma
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(734, 412);
+            this.Controls.Add(this.panelis_spele);
+            this.Controls.Add(this.statusa_josla);
+            this.Controls.Add(this.izvelne);
+            this.MainMenuStrip = this.izvelne;
+            this.Name = "galvena_forma";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Puzlicis";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusa_josla;
+        private System.Windows.Forms.MenuStrip izvelne;
+        private System.Windows.Forms.Timer taimeris;
+        private System.Windows.Forms.Panel panelis_spele;
+    }
+}
+
---
+++ galvena_forma.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace puzlicis
+{
+    public partial class galvena_forma : Form
+    {
+        public gabalins[,] gabalini;
+        
+        public class gabalins
+        {
+            public int indekss;
+            public RectangleF no;
+        }
+
+        public void generet_laukumu()
+        {
+            Bitmap attels = new Bitmap(Image.FromFile("attels_01.jpg"), panelis_spele.Width, panelis_spele.Height);
+            Graphics g = panelis_spele.CreateGraphics();
+            Color krasa = Color.Red;
+
+            int rindu_sk = 4;
+            int kolonnu_sk = 4;
+            int kopa_sk = rindu_sk * kolonnu_sk;
+
+            float platums = panelis_spele.Width / rindu_sk;
+            float augstums = panelis_spele.Height / kolonnu_sk;
+
+            int skaititajs = 1;
+            gabalini = new gabalins[rindu_sk, kolonnu_sk];
+
+            for (int i = 0; i < rindu_sk; i++)
+            {
+                for (int j = 0; j < kolonnu_sk; j++)
+                {
+                    gabalini[i, j] = new gabalins()
+                    {
+                        indekss = skaititajs,
+                        no = new RectangleF(i * platums, j * augstums, platums, augstums)
+                    };
+
+                    skaititajs++;
+                }
+            }
+
+            Random r = new Random();
+            gabalins tmp = new gabalins();
+            int[] r1, r2;
+
+            for (int i = 0; i < kopa_sk * 2; i++)
+            {
+                r1 = new int[2] {r.Next(0, rindu_sk),r.Next(0, kolonnu_sk)};
+                r2 = new int[2] {r.Next(0, rindu_sk),r.Next(0, kolonnu_sk)};
+
+                tmp = gabalini[r1[0], r1[1]];
+                gabalini[r1[0], r1[1]] = gabalini[r2[0], r2[1]];
+                gabalini[r2[0], r2[1]] = tmp;
+            }
+
+            for (int i = 0; i < rindu_sk; i++)
+            {
+                for (int j = 0; j < kolonnu_sk; j++)
+                {
+                    g.DrawImage(attels, new RectangleF(i * platums, j * augstums, platums, augstums), gabalini[i, j].no, GraphicsUnit.Pixel);
+                    //g.DrawString(gabalini[i, j].indekss.ToString(), new Font(FontFamily.GenericSansSerif, 26), Brushes.White, i * platums, j * augstums);
+                    g.DrawRectangle(new Pen(krasa, 2.0f), i * platums, j * augstums, platums, augstums);
+                }
+            }
+        }
+
+        public galvena_forma()
+        {
+            InitializeComponent();
+        }
+
+        private void panelis_spele_MouseClick(object sender, MouseEventArgs e)
+        {
+            generet_laukumu();
+        }
+    }
+}
