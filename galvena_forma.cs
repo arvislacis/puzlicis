6259713e1baaea987be8d78d44988f58b83e8611
Arvis Lācis <arvis.lacis@gmail.com>
2013-12-28 11:23:09 +0200

    Initial Commit
    Pievienotā funkcionalitāte, izmaiņas:
    - pievienotas dažādas krāsu matricas (vienības, pelēktoņu, aktīvā un
    iezīmes);
    - realizēta spēles interaktivitāte un puzles gabaliņu apmainīšana;
    - izveidota vienota funkcija, lai samainītu vietām divus puzles
    gabaliņus.

--- galvena_forma.cs
+++ galvena_forma.cs
@@ -17,15 +17,50 @@ namespace puzlicis
         public Bitmap attels;
         public Image attela_resurss;
         public int attelu_sk = 25, attela_id, platums, augstums, m, n, m_v, n_v;
+        public int[] iezime;
         public static bool jauna_spele = false;
-        public static Color rezga_krasa = Color.Red, aktiva_krasa = Color.White;
+        public static Color rezga_krasa = Color.Red, aktiva_krasa = Color.White, iezimeta_krasa = Color.Green;
         public static int rindu_sk = 5, kolonnu_sk = 5;
 
+        public ColorMatrix aktiva_m = new ColorMatrix(
+            new float[][] {
+                new float[] {1, 0, 0, 0, 0},
+                new float[] {0, 1, 0, 0, 0},
+                new float[] {0, 0, 1, 0, 0},
+                new float[] {0, 0, 0, 1, 0},
+                new float[] {.2f, .2f, .2f, 0, 1}
+            });
+        public ColorMatrix iezimes_m = new ColorMatrix(
+            new float[][] {
+                new float[] {1, 1, 0, 0, 0},
+                new float[] {0, 1, 0, 0, 0},
+                new float[] {0, 1, 1, 0, 0},
+                new float[] {0, 0, 0, 1, 0},
+                new float[] {0, 0, 0, 0, 1}
+            });
+        public ColorMatrix pelektonu_m = new ColorMatrix(
+            new float[][] {
+                new float[] {.3f, .3f, .3f, 0, 0},
+                new float[] {.59f, .59f, .59f, 0, 0},
+                new float[] {.11f, .11f, .11f, 0, 0},
+                new float[] {0, 0, 0, 1, 0},
+                new float[] {0, 0, 0, 0, 1}
+            });
+        public ColorMatrix vienibas_m = new ColorMatrix(
+            new float[][] {
+                new float[] {1, 0, 0, 0, 0},
+                new float[] {0, 1, 0, 0, 0},
+                new float[] {0, 0, 1, 0, 0},
+                new float[] {0, 0, 0, 1, 0},
+                new float[] {0, 0, 0, 0, 1}
+            });
+
         public gabalins[,] laukums;
+        public gabalins tmp;
         public class gabalins
         {
             public int indekss, m, n;
-            public float gamma;
+            public ColorMatrix matrica;
         }
 
         public void laukuma_dati()
@@ -43,7 +78,7 @@ namespace puzlicis
             {
                 for (int j = 0; j < kolonnu_sk; j++)
                 {
-                    laukums[i, j] = new gabalins() { m = i, n = j, gamma = 1.0f };
+                    laukums[i, j] = new gabalins() { m = i, n = j, matrica = vienibas_m };
                 }
             }
 
@@ -59,20 +94,17 @@ namespace puzlicis
             }
 
             Random r = new Random();
-            gabalins tmp = new gabalins();
-            int[] r1, r2;
+            int[] no, uz;
             
             attela_id = r.Next(1, attelu_sk);
             attela_resurss = Image.FromStream(projekts.GetManifestResourceStream("puzlicis.atteli.attels_" + attela_id.ToString() + ".jpg"));
 
             for (int i = 0; i < rindu_sk * kolonnu_sk * 2; i++)
             {
-                r1 = new int[2] { r.Next(0, rindu_sk), r.Next(0, kolonnu_sk) };
-                r2 = new int[2] { r.Next(0, rindu_sk), r.Next(0, kolonnu_sk) };
+                no = new int[2] { r.Next(0, rindu_sk), r.Next(0, kolonnu_sk) };
+                uz = new int[2] { r.Next(0, rindu_sk), r.Next(0, kolonnu_sk) };
 
-                tmp = laukums[r1[0], r1[1]];
-                laukums[r1[0], r1[1]] = laukums[r2[0], r2[1]];
-                laukums[r2[0], r2[1]] = tmp;
+                samainit_gabalinus(no[0], no[1], uz[0], uz[1]);
             }
         }
 
@@ -86,27 +118,9 @@ namespace puzlicis
                 {
                     Rectangle kur_zimet = new Rectangle(i * platums, j * augstums, platums, augstums);
                     ImageAttributes efekti = new ImageAttributes();
-                    efekti.SetGamma(laukums[i, j].gamma);
+                    efekti.SetColorMatrix(laukums[i, j].matrica);
 
-                    // TODO Melnbaltā spēles režīma atbalsts
-                    //efekti.SetColorMatrix(new ColorMatrix(
-                    //    new float[][]{
-                    //        new float[] {.3f, .3f, .3f, 0, 0},
-                    //        new float[] {.59f, .59f, .59f, 0, 0},
-                    //        new float[] {.11f, .11f, .11f, 0, 0},
-                    //        new float[] {0, 0, 0, 1, 0},
-                    //        new float[] {0, 0, 0, 0, 1}
-                    //    }));
-
-                    //efekti.SetColorMatrix(new ColorMatrix(
-                    //    new float[][]{
-                    //        new float[] {1, 0, 0, 0, 0},
-                    //        new float[] {0, 1, 0, 0, 0},
-                    //        new float[] {0, 0, 1, 0, 0},
-                    //        new float[] {0, 0, 0, 1, 0},
-                    //        new float[] {0, 0, 0, 0, 1}
-                    //    }));
-
+                    // TODO Atdalīt zīmēšanas funkciju (līniju) jaunā zīmēšanas funkcijā, lai tai varētu piekļūt ārēji
                     g.DrawImage(attels, kur_zimet, laukums[i, j].m * platums, laukums[i, j].n * augstums, platums, augstums, GraphicsUnit.Pixel, efekti);
                     
                     if (radit_rezgi.Checked)
@@ -122,6 +136,18 @@ namespace puzlicis
             }
 
             g.DrawRectangle(new Pen(aktiva_krasa, 2.0f), m * platums, n * augstums, platums, augstums);
+
+            if (iezime != null)
+            {
+                g.DrawRectangle(new Pen(iezimeta_krasa, 2.0f), iezime[0] * platums, iezime[1] * augstums, platums, augstums);
+            }
+        }
+
+        public void samainit_gabalinus(int no_i, int no_j, int uz_i, int uz_j)
+        {
+            tmp = laukums[no_i, no_j];
+            laukums[no_i, no_j] = laukums[uz_i, uz_j];
+            laukums[uz_i, uz_j] = tmp;
         }
 
         public void noteikt_indeksus(int x, int y)
@@ -170,10 +196,19 @@ namespace puzlicis
             {
                 noteikt_indeksus(e.X, e.Y);
 
-                if (laukums[m, n].gamma == 1.0f)
+                if (laukums[m, n].matrica == iezimes_m)
                 {
-                    laukums[m_v, n_v].gamma = 1.0f;
-                    laukums[m, n].gamma = 0.5f;
+                    zimet_laukumu();
+                }
+
+                if (laukums[m, n].matrica == vienibas_m)
+                {
+                    if (laukums[m_v, n_v].matrica != iezimes_m)
+                    {
+                        laukums[m_v, n_v].matrica = vienibas_m;
+                    }
+
+                    laukums[m, n].matrica = aktiva_m;
                     zimet_laukumu();
                 }
             }
@@ -181,7 +216,24 @@ namespace puzlicis
 
         private void panelis_spele_MouseClick(object sender, MouseEventArgs e)
         {
+            if (jauna_spele)
+            {
+                noteikt_indeksus(e.X, e.Y);
 
+                if (iezime == null)
+                {
+                    iezime = new int[2] { m, n };
+                    laukums[m, n].matrica = iezimes_m;
+                }
+                else
+                {
+                    samainit_gabalinus(iezime[0], iezime[1], m, n);
+                    laukums[iezime[0], iezime[1]].matrica = laukums[m, n].matrica = vienibas_m;
+                    iezime = null;
+                }
+
+                zimet_laukumu();
+            }
         }
 
         private void prieksskatijums_Click(object sender, EventArgs e)
@@ -189,7 +241,7 @@ namespace puzlicis
             if (jauna_spele)
             {
                 prieksskatijuma_forma forma = new prieksskatijuma_forma();
-                forma.BackgroundImage = new Bitmap(Image.FromStream(projekts.GetManifestResourceStream("puzlicis.atteli.attels_" + attela_id.ToString() + ".jpg")), forma.Width, forma.Height);
+                forma.BackgroundImage = new Bitmap(attela_resurss, forma.Width, forma.Height);
                 forma.ShowDialog();
             }
         }
@@ -214,6 +266,7 @@ namespace puzlicis
 
             if (jauna_spele)
             {
+                iezime = null;
                 attēlaPriekšskatījumsToolStripMenuItem.Enabled = radit_rezgi.Visible = radit_indeksus.Visible = true;
                 generet_laukumu();
                 laukuma_dati();
